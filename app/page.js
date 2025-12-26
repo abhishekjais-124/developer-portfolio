8ef160ee4c8abe9f0877fc760482769e2dab55ab
Abhishek Jaiswal <65389204+abhishekjais-124@users.noreply.github.com>
2025-12-27 00:49:33 +0530

    indent fix
    Removed unused export statement for dynamic rendering.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 // export const dynamic = "force-dynamic";
-
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
