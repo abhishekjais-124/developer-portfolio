e63b7b608b4f380a710dc10fba027b1252d8abc4
Abhishek Jaiswal <65389204+abhishekjais-124@users.noreply.github.com>
2025-12-27 00:18:53 +0530

    Remove dynamic export from page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
